fc406237211c8ae20b1610db3e0db7b02636662f
Ishita Shah <ishah@ishah-mn1.linkedin.biz>
2015-08-29 04:46:08 -0700

    discover categories

--- app/src/main/java/com/hackathon/askme/DiscoverActivity.java
+++ app/src/main/java/com/hackathon/askme/DiscoverActivity.java
@@ -1,8 +1,86 @@
 package com.hackathon.askme;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.hackathon.askme.models.Category;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ishah on 8/28/15.
  */
-public class DiscoverActivity {
+public class DiscoverActivity extends ListActivity {
+    private String context;
+    List<Category> categories;
+    CategoryViewAdapter categoryViewAdapter
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // storing string resources into Array
+        String[] adobe_products = getResources().getStringArray(R.array.categories);
+
+        // Binding resources Array to ListAdapter
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.categories_listview, R.id.label, categories));
+
+        ListView lv = getListView();
+
+        // listening to single list item on click
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // selected item
+                String product = ((TextView) view).getText().toString();
+
+                // Launching new Activity on selecting single List Item
+                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                // sending data to new activity
+                i.putExtra("context", "context");
+                startActivity(i);
+
+            }
+        });
+        /*
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        context = intent.getStringExtra("context");
+        if(context.equals("discover")) {
+            ParseQuery<Category> query = ParseQuery.getQuery("Category");
+            query.findInBackground(new FindCallback<Category>() {
+                public void done(List<Category> categoryList, ParseException e) {
+                    if (e == null) {
+                        categories.clear();
+                        categories.addAll(categoryList);
+                        categoryViewAdapter = new CategoryViewAdapter(DiscoverActivity.this, categories);
+                        setContentView();
+                        //objectsWereRetrievedSuccessfully(objects);
+                    } else {
+                        //objectRetrievalFailed();
+                    }
+                }
+            }
+        } else if(context.equals("answer")) {
+
+        } */
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // Do something when a list item is clicked
+    }
 }
--- app/src/main/java/com/hackathon/askme/HomeActivity.java
+++ app/src/main/java/com/hackathon/askme/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.hackathon.askme;
 
+import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -20,13 +22,28 @@ public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity";
 
     @InjectView(R.id.quote) TextView _nameText;
-
+    @InjectView(R.id.action_ask) Button _askButton;
+    @InjectView(R.id.action_answer) Button _answerButton;
+    @InjectView(R.id.action_discover) Button _discoverButton;
+    @InjectView(R.id.action_notifications) Button _notificationsButton;
+    @InjectView(R.id.action_me) Button _meButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
         ButterKnife.inject(this);
+
+        _discoverButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(HomeActivity.this, DiscoverActivity.class);
+                intent.putExtra("context", "discover");
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
