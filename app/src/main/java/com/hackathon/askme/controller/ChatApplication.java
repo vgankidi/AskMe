54ea637ed2ad0d767e5c4e167bd92fb74ea59226
vgankidi <vinitha210@gmail.com>
2015-08-29 08:36:32 -0700

    Working commit

---
+++ app/src/main/java/com/hackathon/askme/controller/ChatApplication.java
@@ -0,0 +1,21 @@
+package com.hackathon.askme.controller;
+
+import android.app.Application;
+
+import com.hackathon.askme.model.Message;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by vigankid on 8/29/15.
+ */
+public class ChatApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "erM6HVhGfEc0AS8LKygVeG26ry5L6mcgfqQvaewl", "TBM5x1u8zIwTQWJRcq1raRAn9IK74sogDuKYGO1I");
+        ParseObject.registerSubclass(Message.class);
+    }
+
+}
