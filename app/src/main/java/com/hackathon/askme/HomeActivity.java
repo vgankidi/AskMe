94c98ac7da0677e2fb0d51f117c2dc87d245658c
Navina <navi.trinity@gmail.com>
2015-08-29 05:29:09 -0700

    Added chat module- needs more tweaking to use local database

--- app/src/main/java/com/hackathon/askme/HomeActivity.java
+++ app/src/main/java/com/hackathon/askme/HomeActivity.java
@@ -1,14 +1,21 @@
 package com.hackathon.askme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.hackathon.askme.controller.ChatActivity;
+import com.hackathon.askme.model.Message;
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -27,6 +34,10 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
         ButterKnife.inject(this);
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "erM6HVhGfEc0AS8LKygVeG26ry5L6mcgfqQvaewl", "TBM5x1u8zIwTQWJRcq1raRAn9IK74sogDuKYGO1I");
+        ParseObject.registerSubclass(Message.class);
     }
 
     @Override
@@ -35,5 +46,18 @@ public class HomeActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.activity_actions, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_ask:
+                Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return false;
+        }
     }
 }
---
+++ app/src/main/java/com/hackathon/askme/controller/ChatListAdapter.java
@@ -0,0 +1,75 @@
+package com.hackathon.askme.controller;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.hackathon.askme.R;
+import com.hackathon.askme.model.Message;
+
+import java.util.List;
+
+public class ChatListAdapter extends ArrayAdapter<Message> {
+    private String mUserId;
+
+    public ChatListAdapter(Context context, String userId, List<Message> messages) {
+        super(context, 0, messages);
+        this.mUserId = userId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).
+                    inflate(R.layout.chat_item, parent, false);
+            final ViewHolder holder = new ViewHolder();
+            holder.textLeft = (TextView)convertView.findViewById(R.id.ivProfileLeft);
+            holder.textRight = (TextView)convertView.findViewById(R.id.ivProfileRight);
+            holder.body = (TextView)convertView.findViewById(R.id.tvBody);
+            convertView.setTag(holder);
+        }
+        final Message message = (Message) getItem(position);
+        final ViewHolder holder = (ViewHolder)convertView.getTag();
+        final boolean isMe = message.getUserId().equals(mUserId);
+        // Show-hide image based on the logged-in user.
+        // Display the profile image to the right for our user, left for other users.
+        if (isMe) {
+            holder.textRight.setVisibility(View.VISIBLE);
+            holder.textLeft.setVisibility(View.GONE);
+            holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT);
+        } else {
+            holder.textLeft.setVisibility(View.VISIBLE);
+            holder.textRight.setVisibility(View.GONE);
+            holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+        }
+//        final TextView profileView = isMe ? holder.textRight : holder.textLeft;
+//        Picasso.with(getContext()).load(getProfileUrl(message.getUserId())).into(profileView);
+        holder.body.setText(message.getBody());
+        return convertView;
+    }
+
+    // Create a gravatar image based on the hash value obtained from userId
+ /*   private static String getProfileUrl(final String userId) {
+        String hex = "";
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("MD5");
+            final byte[] hash = digest.digest(userId.getBytes());
+            final BigInteger bigInt = new BigInteger(hash);
+            hex = bigInt.abs().toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "http://www.gravatar.com/avatar/" + hex + "?d=identicon";
+    }
+*/
+    final class ViewHolder {
+        public TextView textLeft;
+        public TextView textRight;
+        public TextView body;
+    }
+
+}
