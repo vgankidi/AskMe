c235d1823f5c7291763cbc34efdef6776ab2416a
vgankidi <vinitha210@gmail.com>
2015-08-29 11:19:56 -0700

    Working list

--- app/src/main/java/com/hackathon/askme/DiscoverActivity.java
+++ app/src/main/java/com/hackathon/askme/DiscoverActivity.java
@@ -1,21 +1,106 @@
 package com.hackathon.askme;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hackathon.askme.model.Category;
 import com.hackathon.askme.model.CategoryViewAdapter;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.InjectView;
 
 /**
  * Created by ishah on 8/28/15.
  */
+public class DiscoverActivity extends AppCompatActivity {
+    ListView _listView;
+    private List<Category> mCategoryList;
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_discover);
+
+
+    _listView = (ListView) findViewById(R.id.lvDiscover);
+    _listView.setTranscriptMode(1);
+
+
+
+    // assign the list adapter
+
+   /* Category category1 = new Category();
+    category1.setCategory_Id(1);
+    category1.setCategory_Name("Business & Entrepreneurship");
+    Category category2 = new Category();
+    category2.setCategory_Id(2);
+    category2.setCategory_Name("Leadership & Skills");
+    Category category3 = new Category();
+    category3.setCategory_Id(3);
+    category3.setCategory_Name("Career");
+    Category category4 = new Category();
+    category4.setCategory_Id(4);
+    category4.setCategory_Name("Workplace");*/
+    ParseQuery<Category> query = ParseQuery.getQuery(Category.class);
+    query.findInBackground(new FindCallback<Category>() {
+        @Override
+        public void done(List<Category> categories, ParseException e) {
+            if (e == null) {
+                Log.d("categories", "Retrieved " + categories.size() + " categories");
+                CategoryViewAdapter myAdapter = new CategoryViewAdapter(getApplicationContext(), categories);
+                _listView.setAdapter(myAdapter);
+                myAdapter.notifyDataSetChanged();
+            } else {
+                Log.d("categories", "Error: " + e.getMessage());
+            }
+        }
+    });
+
+    
+
+    _listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view,
+                                int position, long id) {
+
+            // ListView Clicked item index
+            int itemPosition = position;
+
+            // ListView Clicked item value
+            Category itemValue = (Category) _listView.getItemAtPosition(position);
+
+            // Show Alert
+            Toast.makeText(getApplicationContext(),
+                    "Position :" + itemPosition + "  ListItem : " + itemValue.getCategory_Name(), Toast.LENGTH_LONG)
+                    .show();
+
+        }
+
+    });
+    }
+}
+
+
+/*
 public class DiscoverActivity extends ListActivity {
     private String context;
     List<Category> categories;
@@ -25,7 +110,7 @@ public class DiscoverActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // storing string resources into Array
-        String[] adobe_products = getResources().getStringArray(R.array.categories);
+      /*  String[] adobe_products = getResources().getStringArray(R.array.categories);
 
         // Binding resources Array to ListAdapter
         //this.setListAdapter(new ArrayAdapter<String>(this, R.layout.categories_listview, R.id.label, categories));
@@ -48,7 +133,7 @@ public class DiscoverActivity extends ListActivity {
 
             }
         });
-        /*
+
         setContentView(R.layout.activity_main);
         Intent intent = getIntent();
         context = intent.getStringExtra("context");
@@ -69,7 +154,7 @@ public class DiscoverActivity extends ListActivity {
             }
         } else if(context.equals("answer")) {
 
-        } */
+        }
     }
 
     @Override
@@ -77,3 +162,4 @@ public class DiscoverActivity extends ListActivity {
         // Do something when a list item is clicked
     }
 }
+*/
