71013fadace977722121f526078868e69a4a943c
vgankidi <vinitha210@gmail.com>
2015-08-29 11:16:21 -0700

    Working list

--- app/src/main/java/com/hackathon/askme/controller/ChatApplication.java
+++ app/src/main/java/com/hackathon/askme/controller/ChatApplication.java
@@ -2,6 +2,7 @@ package com.hackathon.askme.controller;
 
 import android.app.Application;
 
+import com.hackathon.askme.model.Category;
 import com.hackathon.askme.model.Message;
 import com.parse.Parse;
 import com.parse.ParseObject;
@@ -16,6 +17,7 @@ public class ChatApplication extends Application {
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "erM6HVhGfEc0AS8LKygVeG26ry5L6mcgfqQvaewl", "TBM5x1u8zIwTQWJRcq1raRAn9IK74sogDuKYGO1I");
         ParseObject.registerSubclass(Message.class);
+        ParseObject.registerSubclass(Category.class);
     }
 
 }
--- app/src/main/java/com/hackathon/askme/model/CategoryViewAdapter.java
+++ app/src/main/java/com/hackathon/askme/model/CategoryViewAdapter.java
@@ -1,10 +1,17 @@
 package com.hackathon.askme.model;
 
 import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.hackathon.askme.R;
 import com.hackathon.askme.model.Category;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -17,4 +24,24 @@ public class CategoryViewAdapter extends ArrayAdapter<Category> {
         super(context, 0, categories);
     }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.discover_item, parent, false);
+            final ViewHolder viewHolder = new ViewHolder();
+            viewHolder.body = (TextView) convertView.findViewById(R.id.tv_category);
+            convertView.setTag(viewHolder);
+        }
+        final Category category = (Category) getItem(position);
+        final ViewHolder holder = (ViewHolder) convertView.getTag();
+        holder.body.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+        holder.body.setText(category.getCategory_Name());
+
+        return convertView;
+    }
+
+    final class ViewHolder {
+        public TextView body;
+    }
 }
